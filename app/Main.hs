78687dc14796fdb56628b5f645739d70596471b5
Flavio Corpa <flaviocorpa@gmail.com>
2022-01-03 10:59:20 +0100

    Let there be light! 💡

---
+++ app/Main.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Main where
+
+import Control.Applicative
+import Control.Lens
+import Data.Char
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.Text as T
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
